46cb5d070b4629d3551cd7e700e35887c0eefa42
Guilherme Luciano Donin Villaca <guilherme.villaca@unioeste.br>
2016-03-21 11:18:35 -0300

    alterado nomes links sidebar

--- application/views/includes/sidebar.php
+++ application/views/includes/sidebar.php
@@ -2,11 +2,11 @@
           <div class="well sidebar-nav">
             <ul class="nav nav-list">
               <li class="nav-header">Produtos</li>
-              <li <?= active('produtos')?>><a href="#">Gerenciar Produtos</a></li>
-              <li <?= active('produtos')?>><a href="#">Gerenciar Categorias</a></li>
+              <li <?= active('produtos')?>><a href="#">Produtos</a></li>
+              <li <?= active('produtos')?>><a href="#">Categorias</a></li>
               <li class="nav-header">Site</li>
-              <li <?= active('produtos')?>><a href="#">Gerenciar Fotos</a></li>
-              <li <?= active('posts')?>><a href="<?php echo base_url('posts') ?>">Gerenciar Notícias</a></li>
+              <li <?= active('produtos')?>><a href="#">Fotos</a></li>
+              <li <?= active('posts')?>><a href="<?php echo base_url('posts') ?>">Lista de Preços</a></li>
               <li class="nav-header">Admin</li>
               <li <?= active('user')?>><a href="#">Usuários</a></li>
               <li <?= active('config')?>><a href="#">Configurações</a></li>
